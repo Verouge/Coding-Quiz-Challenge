907a9437e9dea4e22feea923e95fab9c9a240396
Verouge <branola1998628@gmail.com>
2023-06-22 23:33:08 +1000

    Update Files

--- js/highscores.js
+++ js/highscores.js
@@ -1,11 +1,29 @@
 const highscoresList = document.getElementById("highscores-list");
+const backBtn = document.getElementById("back-btn");
+const clearBtn = document.getElementById("clear-btn");
 
 // Retrieve scores from local storage
-const scores = JSON.parse(localStorage.getItem("scores")) || [];
+let scores = JSON.parse(localStorage.getItem("scores")) || [];
 
 // Display the scores in the list
-scores.forEach((score, index) => {
-  const li = document.createElement("li");
-  li.textContent = `${index + 1}. ${score.initials}: ${score.score}`;
-  highscoresList.appendChild(li);
+function displayScores() {
+  highscoresList.innerHTML = "";
+
+  scores.forEach((score, index) => {
+    const li = document.createElement("li");
+    li.textContent = `${index + 1}. ${score.initials}: ${score.score}`;
+    highscoresList.appendChild(li);
+  });
+}
+
+displayScores();
+
+backBtn.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+clearBtn.addEventListener("click", function () {
+  scores = [];
+  localStorage.setItem("scores", JSON.stringify(scores));
+  displayScores();
 });
